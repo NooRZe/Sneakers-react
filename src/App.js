3e5c323d6ed1273b64b15381955dd060213c185f
NooRZe <noorze9@gmail.com>
2025-06-03 11:28:48 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 import React from "react";
 import axios from "axios";
-import { Route,  Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import AppContext from "./contex";
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
   const [cartOpened, setCartOpened] = React.useState(false);
-  const [searchValue, setSearchValue] = React.useState('');
+  const [searchValue, setSearchValue] = React.useState("");
   const [favorites, setFavorites] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   /*Запрос на бэк для получения массива данных с mockapi 
@@ -24,33 +24,44 @@ function App() {
     });
     */
     async function fetchData() {
-      const cartResponse = await axios.get('https://680b7472d5075a76d98b2cd7.mockapi.io/cart');
-      const favoritesResponse = await axios.get('https://68136c1e129f6313e2113452.mockapi.io/favorites');
-      const itemsResponse = await axios.get('https://680b7472d5075a76d98b2cd7.mockapi.io/items');
+      const cartResponse = await axios.get(
+        "https://680b7472d5075a76d98b2cd7.mockapi.io/cart"
+      );
+      const favoritesResponse = await axios.get(
+        "https://68136c1e129f6313e2113452.mockapi.io/favorites"
+      );
+      const itemsResponse = await axios.get(
+        "https://680b7472d5075a76d98b2cd7.mockapi.io/items"
+      );
 
       setIsLoading(false);
 
-      setCartItems(cartResponse.data);    
-      setFavorites(favoritesResponse.data);     
-      setItems(itemsResponse.data);   
+      setCartItems(cartResponse.data);
+      setFavorites(favoritesResponse.data);
+      setItems(itemsResponse.data);
     }
 
     fetchData();
-  }, [])
+  }, []);
 
   const onAddToCart = (obj) => {
     console.log(obj);
-    
+
     if (cartItems.find((cartObj) => Number(cartObj.id) === Number(obj.id))) {
-      axios.delete(`https://680b7472d5075a76d98b2cd7.mockapi.io/cart/${obj.id}`);
-      setCartItems((prev) => prev.filter((item) => Number(item.id) !== Number(obj.id)));
+      axios.delete(
+        `https://680b7472d5075a76d98b2cd7.mockapi.io/cart/${obj.id}`
+      );
+      setCartItems((prev) =>
+        prev.filter((item) => Number(item.id) !== Number(obj.id))
+      );
     } else {
-      axios.post('https://680b7472d5075a76d98b2cd7.mockapi.io/cart', obj).then(res => setCartItems((prev) => [...prev, res.data])) 
+      axios
+        .post("https://680b7472d5075a76d98b2cd7.mockapi.io/cart", obj)
+        .then((res) => setCartItems((prev) => [...prev, res.data]));
     }
-    
-    
-    //axios.post('https://680b7472d5075a76d98b2cd7.mockapi.io/cart', obj).then(res =>  setCartItems((prev) => [...prev, res.data]))     
-    
+
+    //axios.post('https://680b7472d5075a76d98b2cd7.mockapi.io/cart', obj).then(res =>  setCartItems((prev) => [...prev, res.data]))
+
     /* Вариант как с избранным
     if (cartItems.find((favObj) => favObj.id === obj.id)) {
       axios.delete(`https://680b7472d5075a76d98b2cd7.mockapi.io/cart/${obj.id}`);  
@@ -67,57 +78,65 @@ function App() {
 
   const onAddToFavorites = (obj) => {
     if (favorites.find((favObj) => favObj.id === obj.id)) {
-      axios.delete(`https://68136c1e129f6313e2113452.mockapi.io/favorites/${obj.id}`);  
-      setFavorites((prev) => prev.filter((item) => item.id !== obj.id))
+      axios.delete(
+        `https://68136c1e129f6313e2113452.mockapi.io/favorites/${obj.id}`
+      );
+      setFavorites((prev) => prev.filter((item) => item.id !== obj.id));
     } else {
-      axios.post('https://68136c1e129f6313e2113452.mockapi.io/favorites', obj).then(res => setFavorites((prev) => [...prev, res.data]))     
+      axios
+        .post("https://68136c1e129f6313e2113452.mockapi.io/favorites", obj)
+        .then((res) => setFavorites((prev) => [...prev, res.data]));
     }
   };
 
   const onRemoveItem = (obj) => {
     axios.delete(`https://680b7472d5075a76d98b2cd7.mockapi.io/cart/${obj.id}`);
     setCartItems((prev) => prev.filter((item) => item.id !== obj.id));
-  }
+  };
 
   //реализация поиска
   const onChangeSearchInput = (event) => {
     setSearchValue(event.target.value);
-  } 
+  };
 
   return (
-    <AppContext.Provider value={{cartItems, favorites, items}}>
+    <AppContext.Provider value={{ cartItems, favorites, items }}>
       <div className="wrapper clear">
-        {cartOpened && <Drawer 
-          items= {cartItems} 
-          onClose={() => setCartOpened(false)} 
-          onRemove={onRemoveItem}
-        />}
-        <Header onClickCart={() => setCartOpened(true)}/>
+        {cartOpened && (
+          <Drawer
+            items={cartItems}
+            onClose={() => setCartOpened(false)}
+            onRemove={onRemoveItem}
+          />
+        )}
+        <Header onClickCart={() => setCartOpened(true)} />
 
         <Routes>
-          <Route path="/" element = {
-            <Home
-              cartItems = {cartItems}
-              items = {items}
-              searchValue = {searchValue}
-              setSearchValue = {setSearchValue}
-              onChangeSearchInput = {onChangeSearchInput}
-              onAddToFavorites = {onAddToFavorites}
-              onAddToCart = {onAddToCart}
-              isLoading = {isLoading}
-              favorites = {favorites}
-            />
-          } exact>
-          </Route>
+          <Route
+            path="/"
+            element={
+              <Home
+                cartItems={cartItems}
+                items={items}
+                searchValue={searchValue}
+                setSearchValue={setSearchValue}
+                onChangeSearchInput={onChangeSearchInput}
+                onAddToFavorites={onAddToFavorites}
+                onAddToCart={onAddToCart}
+                isLoading={isLoading}
+                favorites={favorites}
+              />
+            }
+            exact
+          ></Route>
         </Routes>
 
         <Routes>
-          <Route path="/favorites" element = {
-            <Favorites
-            onAddToFavorites = {onAddToFavorites}
-            />
-          } exact>
-          </Route>
+          <Route
+            path="/favorites"
+            element={<Favorites onAddToFavorites={onAddToFavorites} />}
+            exact
+          ></Route>
         </Routes>
       </div>
     </AppContext.Provider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Header(props) {
-  return(
+  return (
     <header className="d-flex justify-between align-center p-40">
       <Link to="/">
-      <div className="d-flex align-center">
-        <img width={40} height={40} src="/img/logo.png" alt="logo"/>
-        <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
-          <p className="opacity-5">Магазин лучших кроссовок</p>
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" alt="logo" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
+          </div>
         </div>
-      </div>
       </Link>
       <ul className="d-flex">
         <li onClick={props.onClickCart} className="mr-20 cu-p">
-          <img width={18} height={18} src="/img/cart.svg" alt="cart"/> 
+          <img width={18} height={18} src="/img/cart.svg" alt="cart" />
           <span>1205 руб.</span>
         </li>
         <li className="mr-30 cu-p">
           <Link to="/favorites">
-            <img  width={18} height={18} src="/img/favorite.svg" alt="favorites"/>
+            <img
+              width={18}
+              height={18}
+              src="/img/favorite.svg"
+              alt="favorites"
+            />
           </Link>
         </li>
         <li>
-          <img width={18} height={18} src="/img/user.svg" alt="user"/>
+          <img width={18} height={18} src="/img/user.svg" alt="user" />
         </li>
       </ul>
-     </header>
+    </header>
   );
 }
 
